36d17821d0d10f4cc12aee3cf0121d3dbb4e8314
DavidKross <mylyfwy771@gmail.com>
2014-09-10 15:40:57 +0800

    Inital commit & GetSelect json data.

---
+++ js/index.js
@@ -0,0 +1,48 @@
+/* *********************************
+[area.code] json data
+
+台北市 [ 1 to 12 ]
+基隆市 [ 13 to 19 ]
+新北市 [20, 21] ++ [ 26 to 52]
+新竹市 [53]
+新竹縣 [54 to 66]
+桃園縣 [67 to 79]
+苗栗縣 [80 to 97]
+台中市 [98 to 126]
+彰化縣 [127 to 152]
+南投縣 [153 to 165]
+雲林縣 [185 to 204]
+嘉義市 [166]
+嘉義縣 [167 to 184]
+臺南市 [206 to 242]
+高雄市 [243 to 282]
+屏東縣 [295 to 327]
+宜蘭縣 [328 to 339]
+花蓮縣 [357 to 369]
+台東縣 [341 to 356]
+澎湖縣 [283 to 288]
+金門縣 [289 to 294]
+連江縣 [22 to 25] ++ [256, 257]
+
+********************************* */
+
+$(document).ready(function(){
+
+	var jsonFile;
+
+	$.ajax({
+		async: false,
+		dataType: "json",
+		url: 'json/1.json',
+		success: function(data){
+			jsonFile = data;
+		}
+	});
+
+	function search_area(selector){
+		var obj = JSONSelect.match(selector , jsonFile);
+		return obj
+	}
+
+	console.log(search_area('."2004" ."地址"').length);
+});
